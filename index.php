7d33784e49a309864d908a9e275e8766ab870423
Hideki Higuchi <hideki-higuchi@outlook.com>
2015-06-10 10:15:40 -0300

    cya

--- index.php
+++ index.php
@@ -12,5 +12,41 @@
             <input type="radio" name="tipoCRUD" value="D"/>Delete<br/>
             <input type="submit" value="START"/>
         </form>
+        
+        <style type="text/css">
+.tg  {border-collapse:collapse;border-spacing:0;}
+.tg td{font-family:Arial, sans-serif;font-size:14px;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
+.tg th{font-family:Arial, sans-serif;font-size:14px;font-weight:normal;padding:10px 5px;border-style:solid;border-width:1px;overflow:hidden;word-break:normal;}
+</style>
+        
+        
+        <table class="tg">
+  <tr>
+    <th class="tg-s6z2">Tia</th>
+    <th class="tg-031e">Nome</th>
+    <th class="tg-031e">Nota</th>
+    <th class="tg-031e">Idade</th>
+    <th class="tg-031e"><a href="crud.php?tipo_crud=C">Novo</a></th>
+  </tr>
+  <tr>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"><a href="crud.php?tipo_crud=U&tia=XXX">E</a><a href="crud.php?tipo_crud=D&tia=XXX"> X</a></td>
+  </tr>
+  <tr>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"></td>
+    <td class="tg-031e"><a href="crud.php?tipo_crud=U&tia=XXX">E</a><a href="crud.php?tipo_crud=D&tia=XXX"> X</a></td>
+                            
+       //紛らわしい
+      
+  </tr>
+</table>
+        
+        
     </body>
 </html>
